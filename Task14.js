1d8526ecfdcecc351d7f0985713488e317b729a0
mahmoud <mahmoudmuhdmm@gmail.com>
2025-11-16 11:02:03 +0100

    Add README file

--- Task14.js
+++ Task14.js
@@ -1,22 +1,4 @@
-let temp = window.prompt("Enter temperature:  ?");
-if (temp = 30){
-    console.log("hot");
-}
-else if (temp = 20 && temp < 30){
-    console.log("warm");
-}
-else if (temp < 20){
-    console.log("cold");
-}
-
-else {
-    console.log("unknown temperature");
-};
-
-
-
-
-let temp = Number(window.prompt("Enter temperature: ?")); // Convert input to number
+let temp = Number(window.prompt("Enter temperature: ?"));
 
 if (temp === 30) {
     console.log("hot");
@@ -28,4 +10,4 @@ if (temp === 30) {
     console.log("unknown temperature");
 }
 
-// corrected version with external help.
+
